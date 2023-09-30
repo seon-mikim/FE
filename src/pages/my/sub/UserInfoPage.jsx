8f9a73885981143994d96a44064c1ba323b7577e
seon-mikim <tjsalsun@gmail.com>
2023-09-30 21:38:10 +0900

    feat: UserSaleListSection 컴포넌트 구현

--- src/pages/my/sub/UserInfoPage.jsx
+++ src/pages/my/sub/UserInfoPage.jsx
@@ -1,14 +1,16 @@
 import UserCartContent from '../../../components/MyPageContents/UserCartContent';
 import UserProfileContent from '../../../components/MyPageContents/UserProfileContent';
+import UserSaleItemsContent from '../../../components/MyPageContents/UserSaleItemsContent';
 import MyPageSection from '../../../components/MypageSections';
 const UserInfoPage = () => {
   const UserProfileSection = MyPageSection(UserProfileContent);
   const UserCartListSection = MyPageSection(UserCartContent);
+  const UserSaleListSection = MyPageSection(UserSaleItemsContent);
   return (
     <>
-      
       <UserProfileSection titleText="내 정보" />
       <UserCartListSection titleText="장바구니" />
+      <UserSaleListSection titleText="내가 파는 상품" />
     </>
   );
 };
