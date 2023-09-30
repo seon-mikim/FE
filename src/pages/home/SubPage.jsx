4290a2ca7aac2f08b21abc9ac4f3ae4d54c59f77
denshou <132555762+denshou@users.noreply.github.com>
2023-10-01 01:27:49 +0900

    feat: 동적 페이지네이션
    product 수량에 따라 동적으로 페이지네이션 생성
    맨앞, 이전, 다음, 맨끝 버튼 구현
    카테고리 버튼 css만 구현 / product에 카테고리 옵션 추가해서 구분 필요

--- src/pages/home/SubPage.jsx
+++ src/pages/home/SubPage.jsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 import "./subpage.css";
 import CardLi from "../../components/Card/CardLi";
+import { useState } from "react";
 const SubPage = () => {
-  const first = "/first.gif";
-  const prev = "/prev.gif";
-  const next = "/next.gif";
-  const end = "/end.gif";
+  const [page, setPage] = useState(1);
+  const limit = 4;
+  const offset = (page - 1) * limit;
+
+  const [category, setCategory] = useState("");
 
   const productList = [
     {
-      id: "P00000KP",
+      id: "P00000KA",
       name: "Denim Shirt Jacket-Black",
       price: 119000,
       img: ["/p1.jpg", "/p1-2.png"],
@@ -27,7 +29,7 @@ const SubPage = () => {
       totalStock: 15,
     },
     {
-      id: "P00000KO",
+      id: "P00000KB",
       name: "GOALSTUDIO Delight Tote Bag",
       price: 95200,
       img: ["/p2.jpg"],
@@ -35,7 +37,7 @@ const SubPage = () => {
       totalStock: 10,
     },
     {
-      id: "P00000KQ",
+      id: "P00000KC",
       name: "Denim Shirt Jacket-Blue",
       price: 119000,
       img: ["/p3.jpg", "/p3-2.png"],
@@ -54,14 +56,101 @@ const SubPage = () => {
     },
 
     {
-      id: "P00000KA",
+      id: "P00000KD",
       name: "GOALSTUDIO Delight Bag",
       price: 119000,
       img: ["/p4.jpg", "/p4-2.png"],
       buyQuantity: 0,
       totalStock: 3,
     },
+
+    {
+      id: "P00000KE",
+      name: "Denim Shirt Jacket-Black",
+      price: 119000,
+      img: ["/p1-2.png", "/p1.jpg"],
+      buySize: {
+        s: 0,
+        m: 0,
+        l: 0,
+      },
+      buyQuantity: 0,
+      stock: {
+        s: 5,
+        m: 6,
+        l: 4,
+      },
+      totalStock: 15,
+    },
+    {
+      id: "P00000KF",
+      name: "GOALSTUDIO Delight Tote Bag",
+      price: 95200,
+      img: ["/p2.jpg"],
+      buyQuantity: 0,
+      totalStock: 10,
+    },
+    {
+      id: "P00000KG",
+      name: "Denim Shirt Jacket-Blue",
+      price: 119000,
+      img: ["/p3-2.png", "/p3.jpg"],
+      buySize: {
+        s: 0,
+        m: 0,
+        l: 0,
+      },
+      buyQuantity: 0,
+      stock: {
+        s: 5,
+        m: 6,
+        l: 4,
+      },
+      totalStock: 15,
+    },
+
+    {
+      id: "P00000KH",
+      name: "GOALSTUDIO Delight Bag",
+      price: 119000,
+      img: ["/p4-2.png", "/p4.jpg"],
+      buyQuantity: 0,
+      totalStock: 3,
+    },
   ];
+
+  const maxPage = productList.length / limit;
+
+  const sliceData = (data) => {
+    if (data) {
+      let result = data.slice(offset, offset + limit);
+      return result;
+    }
+  };
+
+  const changePage = (e) => {
+    setPage(Number(e.target.value));
+  };
+  const changePagination = (e) => {
+    switch (e.target.value) {
+      case "first":
+        setPage(1);
+        break;
+      case "prev":
+        setPage((prev) => prev - 1);
+        break;
+      case "next":
+        setPage((prev) => prev + 1);
+        break;
+      case "end":
+        setPage(maxPage);
+        break;
+    }
+  };
+  const changeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
   const renderCard = (product) => {
     return (
       <CardLi
@@ -74,46 +163,112 @@ const SubPage = () => {
       />
     );
   };
+  const renderPagination = () => {
+    const result = [];
+    for (let i = 0; i < maxPage; i++) {
+      if (i === 0) {
+        result.push(
+          <li className="page-first" key="1">
+            <button
+              className={page === 1 ? "selected" : "num"}
+              value="1"
+              onClick={changePage}
+            >
+              1
+            </button>
+          </li>
+        );
+      } else {
+        result.push(
+          <li className="page" key={i + 1}>
+            <button
+              className={page === i + 1 ? "selected" : "num"}
+              value={i + 1}
+              onClick={changePage}
+            >
+              {i + 1}
+            </button>
+          </li>
+        );
+      }
+    }
+    return result;
+  };
 
   return (
     <Section>
       <div className="sub-title">
         <div className="sub-list-title">APPAREL</div>
         <div className="sub-list-select">
-          <div className="sub-list-btn">TOPS & T-SHIRTS</div>
-          <div className="sub-list-btn">HOODIES & SWEATSHIRTS</div>
-          <div className="sub-list-btn">PANTS</div>
-          <div className="sub-list-btn">BUNDLE</div>
+          <button
+            className={
+              category === "TOPS & T-SHIRTS"
+                ? "sub-list-btn-selected"
+                : "sub-list-btn"
+            }
+            value="TOPS & T-SHIRTS"
+            onClick={changeCategory}
+          >
+            TOPS & T-SHIRTS
+          </button>
+          <button
+            className={
+              category === "HOODIES & SWEATSHIRTS"
+                ? "sub-list-btn-selected"
+                : "sub-list-btn"
+            }
+            value="HOODIES & SWEATSHIRTS"
+            onClick={changeCategory}
+          >
+            HOODIES & SWEATSHIRTS
+          </button>
+          <button
+            className={
+              category === "PANTS" ? "sub-list-btn-selected" : "sub-list-btn"
+            }
+            value="PANTS"
+            onClick={changeCategory}
+          >
+            PANTS
+          </button>
+          <button
+            className={
+              category === "BUNDLE" ? "sub-list-btn-selected" : "sub-list-btn"
+            }
+            value="BUNDLE"
+            onClick={changeCategory}
+          >
+            BUNDLE
+          </button>
         </div>
       </div>
-      <ItemListGrid>{productList.map(renderCard)}</ItemListGrid>
+      <ItemListGrid>{sliceData(productList).map(renderCard)}</ItemListGrid>
       <div className="pagination">
-        <div>
-          <img src={first} alt="" />
-        </div>
-        <div>
-          <img src={prev} alt="" />
-        </div>
-        <ol>
-          <li className="page-first">
-            <div className="num-selected">1</div>
-          </li>
-          <li className="page">
-            <div className="num">2</div>
-          </li>
-          <li className="page">
-            <div className="num">3</div>
-          </li>
-          <li className="page">
-            <div className="num">4</div>
-          </li>
-        </ol>
-        <div>
-          <img src={next} alt="" />
-        </div>
-        <div>
-          <img src={end} alt="" />
-        </div>
+        <button
+          className="first-page"
+          value="first"
+          onClick={changePagination}
+          disabled={page === 1 ? true : false}
+        ></button>
+        <button
+          className="prev-page"
+          value="prev"
+          onClick={changePagination}
+          disabled={page === 1 ? true : false}
+        ></button>
+        <ol>{renderPagination()}</ol>
+        <button
+          className="next-page"
+          value="next"
+          onClick={changePagination}
+          disabled={page === maxPage ? true : false}
+        ></button>
+        <button
+          className="end-page"
+          value="end"
+          onClick={changePagination}
+          disabled={page === maxPage ? true : false}
+        ></button>
       </div>
     </Section>
   );
