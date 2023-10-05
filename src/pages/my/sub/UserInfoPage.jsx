6039434d11c1b423d235f9f14afa12d9827318b5
seon-mikim <tjsalsun@gmail.com>
2023-10-05 22:34:49 +0900

    comment: 파일 삭제로 인해 해당 부분 주석 처리

--- src/pages/my/sub/UserInfoPage.jsx
+++ src/pages/my/sub/UserInfoPage.jsx
@@ -1,19 +1,20 @@
 
-import { UserCartList, UserOrderList, UserProfileList, UserSalesList } from '../../../components/MypageList';
+// import UserCartContent from '../../../components/MyPageContents/UserCartContent';
+// import { UserOrderList, UserProfileList, UserSalesList } from '../../../components/MypageList';
 
-import MyPageSection from '../../../components/MypageSections';
+// import MyPageSection from '../../../components/MypageSections';
 const UserInfoPage = () => {
-  const UserProfileSection = MyPageSection(UserProfileList);
-  const UserCartListSection = MyPageSection(UserCartList);
-  const UserSaleListSection = MyPageSection(UserSalesList);
-  const UserOrderListSection = MyPageSection(UserOrderList);
+  // const UserProfileSection = MyPageSection(UserProfileList);
+  // const UserCartListSection = MyPageSection(UserCartContent);
+  // const UserSaleListSection = MyPageSection(UserSalesList);
+  // const UserOrderListSection = MyPageSection(UserOrderList);
   return (
     <>
-      
+{/*       
     <UserProfileSection titleText="내 정보" />
     <UserCartListSection titleText="장바구니" />
     <UserSaleListSection titleText="내가 올린 상품" />
-    <UserOrderListSection titleText="구매 상품" />
+    <UserOrderListSection titleText="구매 상품" /> */}
   
     </>
   );
