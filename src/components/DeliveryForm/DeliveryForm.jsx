5a9f3842759aa6496b28fb76d47bcae29f2e7c19
seon-mikim <tjsalsun@gmail.com>
2023-10-08 22:00:09 +0900

    feat: DeliverForm 컴포넌트 구현

---
+++ src/components/DeliveryForm/DeliveryForm.jsx
@@ -0,0 +1,168 @@
+import styled from 'styled-components';
+import Input from '../ui/Input/Input';
+import { useState } from 'react';
+import PostCodeButton from '../PostCodeButton/PostCodeButton';
+
+const DeliveryForm = () => {
+  const [isShown, setIsShown] = useState(true);
+  const [fullPhoneNumber, setFullPhoneNumber] = useState('');
+  const [phoneInput, setPhoneInput] = useState({
+    fristPhoneNumber: '',
+    secondPhoneNumber: '',
+    thridPhoneNumber: '',
+  });
+  const [deliveryInput, setDeliveryInput] = useState({
+    recipient: '',
+    recipient_tel: '',
+    recipient_address: '',
+    recipient_zipcode: '',
+    recipient_detail_address: '',
+  });
+  const handleDevlieInputChange = (event) => {
+    const { name, value } = event.target;
+    setDeliveryInput({ ...deliveryInput, [name]: value });
+  };
+  const handlePhoneInputChange = (event) => {
+    const { name, value } = event.target;
+    setPhoneInput({ ...phoneInput, [name]: value });
+    setFullPhoneNumber(
+      `${phoneInput.fristPhoneNumber}-${phoneInput.secondPhoneNumber}-${phoneInput.thridPhoneNumber}`
+    );
+    setDeliveryInput({ ...deliveryInput, recipient_tel: fullPhoneNumber });
+  };
+  const getAddress = (recipient_address, recipient_zipcode) => {
+    setDeliveryInput({
+      ...deliveryInput,
+      recipient_address,
+      recipient_zipcode,
+    });
+  };
+  const handleAccordionButton = () => {
+    setIsShown((prev) => !prev);
+  };
+  return (
+    <DeliveryFormWrap>
+      <AccordionHeader>
+        <div>배송지</div>
+        <button onClick={handleAccordionButton}>입력하기</button>
+      </AccordionHeader>
+      <>
+        {isShown && (
+          <DeliveryFormContent>
+            <label>
+              수령인
+              <Input
+                placeholder="수령인"
+                name="recipient"
+                value={deliveryInput.recipient}
+                onChange={handleDevlieInputChange}
+              />
+            </label>
+            <label>
+              연락처
+              <TelInputWrap name="">
+                <TelInput
+                  placeholder="010"
+                  name="fristPhoneNumber"
+                  value={phoneInput.fristPhoneNumber}
+                  onChange={handlePhoneInputChange}
+                />
+                <div>-</div>
+                <TelInput
+                  placeholder="1234"
+                  name="secondPhoneNumber"
+                  value={phoneInput.secondPhoneNumber}
+                  onChange={handlePhoneInputChange}
+                />
+                <div>-</div>
+                <TelInput
+                  placeholder="5678"
+                  name="thridPhoneNumber"
+                  value={phoneInput.thridPhoneNumber}
+                  onChange={handlePhoneInputChange}
+                />
+              </TelInputWrap>
+            </label>
+            <label>
+              배송지
+              <div>
+                <div>
+                  <PostCodeInput
+                    placeholder="우편번호"
+                    name="recipient_zipcode"
+                    value={deliveryInput.recipient_zipcode}
+                    onChange={handleDevlieInputChange}
+                  />
+                  <PostCodeButton getAdress={getAddress} />
+                </div>
+                <div>
+                  <AddressInput
+                    placeholder="주소"
+                    name="recipient_address"
+                    value={deliveryInput.recipient_address}
+                    onChange={handleDevlieInputChange}
+                  />
+                </div>
+                <div>
+                  <DetailAddressInput
+                    name="recipient_detail_address"
+                    placeholder="상세 주소"
+                    onChange={handleDevlieInputChange}
+                  />
+                </div>
+              </div>
+            </label>
+          </DeliveryFormContent>
+        )}
+      </>
+    </DeliveryFormWrap>
+  );
+};
+
+export default DeliveryForm;
+
+const DeliveryFormWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 76%;
+`;
+
+const AccordionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #e2e2e2;
+  padding: 20px;
+`;
+const DeliveryFormContent = styled.div`
+  padding: 20px;
+  border: 1px solid #e2e2e2;
+  label {
+    display: flex;
+    margin: 10px;
+    gap: 30px;
+    input {
+      height: 30px;
+    }
+  }
+`;
+
+const TelInput = styled(Input)`
+  width: 40px;
+  text-align: center;
+`;
+const TelInputWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PostCodeInput = styled(Input)`
+  width: 80px;
+  margin-bottom: 10px;
+`;
+const AddressInput = styled(Input)`
+  width: 350px;
+  margin-bottom: 10px;
+`;
+const DetailAddressInput = styled(Input)`
+  width: 350px;
+`;
