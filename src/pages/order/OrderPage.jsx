9bd9174b76e30150fc422939dcf483c97d38551b
seon-mikim <tjsalsun@gmail.com>
2023-10-08 22:01:22 +0900

    feat: DeliveryForm 컴포넌트 추가

--- src/pages/order/OrderPage.jsx
+++ src/pages/order/OrderPage.jsx
@@ -1,21 +1,36 @@
-// import styled from 'styled-components';
-// import T from '../../assets/images/black_t.jpg';
-// import { useState } from 'react';
 import Main from '../../components/ui/Main/Main';
-// import Wrap from '../../components/ui/Wrap/Wrap';
 import PageSection from '../../components/PageSection/index';
-
+import DeliveryForm from '../../components/DeliveryForm/DeliveryForm';
+import PurchaseCard from '../../components/PurchaseCard/PurchaseCard';
+import T from '../../assets/images/black_t.jpg';
+import styled from 'styled-components';
+import Wrap from '../../components/ui/Wrap/Wrap';
 const OrderPage = () => {
-  
+  const userCartData = [
+    {
+      productId: 1,
+      product_name: '티셔츠',
+      product_img: T,
+      count: 1,
+      price: 10000,
+      option: 's',
+    },
+  ];
   return (
     <Main>
       <PageSection titleText="주문하기">
-
+        <OrderContent>
+          <DeliveryForm />
+          <PurchaseCard userCartData={userCartData} />
+        </OrderContent>
       </PageSection>
-      
     </Main>
   );
 };
 
 export default OrderPage;
 
+const OrderContent = styled(Wrap)`
+  display: flex;
+  gap: 70px;
+`;
