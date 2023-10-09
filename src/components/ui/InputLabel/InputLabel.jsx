863cf4276efb63d9f2791c3b94111ac4de5590c7
seon-mikim <tjsalsun@gmail.com>
2023-10-09 18:45:36 +0900

    refator: 가독성과 재사용을 위한 컴포넌트 분할

--- src/components/ui/InputLabel/InputLabel.jsx
+++ src/components/ui/InputLabel/InputLabel.jsx
@@ -1,12 +1,19 @@
+import { forwardRef } from 'react';
 import Input from '../Input/Input';
-
-const InputLabel = ({ label, type, name, value, ...props }, ref) => {
-  return (
-    <label>
-      {label}
-      <Input {...props} type={type} name={name} value={value} ref={ref} />
-    </label>
-  );
-};
+import Text from '../Text/Text';
+const InputLabel = forwardRef(
+  ({ label, type, name, value, alertText, ...props }, ref) => {
+    console.log(alertText);
+    return (
+      <label>
+        {label}
+        <div>
+          <Input {...props} type={type} name={name} value={value} ref={ref} />
+          <Text text={alertText} />
+        </div>
+      </label>
+    );
+  }
+);
 
 export default InputLabel;
