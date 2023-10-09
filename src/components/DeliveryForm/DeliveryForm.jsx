8723bb68e94d7fdcb7c59e1ee970a62244e6eb00
seon-mikim <tjsalsun@gmail.com>
2023-10-09 18:46:30 +0900

    refactor: 컴포넌트 분할에 따른 변경

--- src/components/DeliveryForm/DeliveryForm.jsx
+++ src/components/DeliveryForm/DeliveryForm.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import Input from '../ui/Input/Input';
 import { useState } from 'react';
-import PostCodeButton from '../PostCodeButton/PostCodeButton';
 import UseDeliveryForm from '../../hooks/UseDeliveryForm';
-import Text from '../ui/Text/Text';
+import InputLabel from '../ui/InputLabel/InputLabel';
+import PhoneInput from '../ui/PhoneInput/PhoneInput';
+import AddressInput from '../ui/AddressInput/AddressInput';
 
 const DeliveryForm = () => {
   const [isShown, setIsShown] = useState(true);
@@ -30,87 +30,28 @@ const DeliveryForm = () => {
       <>
         {isShown && (
           <DeliveryFormContent>
-            <label>
-              수령인
-              <div>
-                <Input
-                  placeholder="수령인"
-                  name="recipient"
-                  value={deliveryInput.recipient}
-                  onChange={handleDevlieInputChange}
-                  ref={(ref) => addRef('recipient', ref)}
-                  onBlur={() => validateInputValue('recipient')}
-                />
-                <Text text={alertMessage.recipient} />
-              </div>
-            </label>
-            <label>
-              연락처
-              <TelWrap name="">
-                <TelInputWrap>
-                  <TelInput
-                    placeholder="010"
-                    name="fristPhoneNumber"
-                    value={phoneInput.fristPhoneNumber}
-                    onChange={handlePhoneInputChange}
-                    onBlur={() => validateInputValue('recipient_tel')}
-                    ref={(ref) => addRef('recipient_tel', ref)}
-                  />
-                  <div>-</div>
-                  <TelInput
-                    placeholder="1234"
-                    name="secondPhoneNumber"
-                    value={phoneInput.secondPhoneNumber}
-                    onChange={handlePhoneInputChange}
-                    onBlur={() => validateInputValue('recipient_tel')}
-                    ref={(ref) => addRef('recipient_tel', ref)}
-                  />
-                  <div>-</div>
-                  <TelInput
-                    placeholder="5678"
-                    name="thridPhoneNumber"
-                    value={phoneInput.thridPhoneNumber}
-                    onChange={handlePhoneInputChange}
-                    onBlur={() => validateInputValue('recipient_tel')}
-                    ref={(ref) => addRef('recipient_tel', ref)}
-                  />
-                </TelInputWrap>
-                <Text text={alertMessage.recipient_tel} />
-              </TelWrap>
-            </label>
-            <label>
-              배송지
-              <div>
-                <div>
-                  <PostCodeInput
-                    placeholder="우편번호"
-                    name="recipient_zipcode"
-                    value={deliveryInput.recipient_zipcode}
-                    onChange={handleDevlieInputChange}
-                  />
-                  <PostCodeButton getAdress={getAddress} />
-                </div>
-                <div>
-                  <AddressInput
-                    placeholder="주소"
-                    name="recipient_address"
-                    value={deliveryInput.recipient_address}
-                    onChange={handleDevlieInputChange}
-                  />
-                </div>
-                <div>
-                  <DetailAddressInput
-                    name="recipient_detail_address"
-                    value={deliveryInput.recipient_detail_address}
-                    placeholder="상세주소"
-                    onChange={handleDevlieInputChange}
-                  />
-                </div>
-                <div>
-                  <Text />
-                </div>
-              </div>
-            </label>
+            <InputLabel
+              label="수령인"
+              placeholder="수령인"
+              name="recipient"
+              value={deliveryInput.recipient}
+              onChange={handleDevlieInputChange}
+              ref={(ref) => addRef('recipient', ref)}
+              onBlur={() => validateInputValue('recipient')}
+              alertText={alertMessage.recipient}
+            />
+            <PhoneInput
+              phoneInput={phoneInput}
+              handlePhoneInputChange={handlePhoneInputChange}
+              validateInputValue={() => validateInputValue('recipient_tel')}
+              ref={(ref) => addRef('recipient_tel', ref)}
+              alertMessage={alertMessage.recipient_tel}
+            />
+            <AddressInput
+              deliveryInput={deliveryInput}
+              handleDevlieInputChange={handleDevlieInputChange}
+              getAddress={getAddress}
+            />
           </DeliveryFormContent>
         )}
       </>
@@ -138,46 +79,16 @@ const DeliveryFormContent = styled.div`
   border: 1px solid #e2e2e2;
   label {
     display: flex;
-    margin: 10px;
     gap: 30px;
     div {
       p {
+        margin-top: 10px;
         height: 20px;
+        color: #ed5454;
       }
     }
     input {
       height: 30px;
     }
   }
-`;
-
-const TelInput = styled(Input)`
-  width: 40px;
-  text-align: center;
-`;
-const TelInputWrap = styled.div`
-  display: flex;
-  align-items: center;
-  p {
-    height: 20px;
-  }
-`;
-const TelWrap = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  p {
-    height: 20px;
-  }
-`;
-const PostCodeInput = styled(Input)`
-  width: 80px;
-  margin-bottom: 10px;
-`;
-const AddressInput = styled(Input)`
-  width: 350px;
-  margin-bottom: 10px;
-`;
-const DetailAddressInput = styled(Input)`
-  width: 350px;
 `;
