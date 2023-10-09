cfd048c87bfb58d90fea37bda2c9b873a9ca4d28
seon-mikim <tjsalsun@gmail.com>
2023-10-09 20:30:11 +0900

    feat: OrderPage 구현

--- src/components/OrderPageContent/OrderPageContent.jsx
+++ src/components/OrderPageContent/OrderPageContent.jsx
@@ -1,5 +1,34 @@
+import DeliveryForm from '../DeliveryForm/DeliveryForm';
+import PurchaseCard from '../PurchaseCard/PurchaseCard';
+import PaymentForm from '../PaymentForm/PaymentForm';
+import T from '../../assets/images/black_t.jpg';
+import styled from 'styled-components';
+import Wrap from '../ui/Wrap/Wrap';
+
 const OrderPageContent = () => {
-  return <div>OrderPageContent</div>;
+  const userCartData = [
+    {
+      productId: 1,
+      product_name: '티셔츠',
+      product_img: T,
+      count: 1,
+      price: 10000,
+      option: 's',
+    },
+  ];
+  return (
+    <>
+      <FormWrap>
+        <DeliveryForm />
+        <PaymentForm />
+      </FormWrap>
+      <PurchaseCard userCartData={userCartData} />
+    </>
+  );
 };
 
 export default OrderPageContent;
+
+const FormWrap = styled(Wrap)`
+  width: 76%;
+`;
--- src/pages/order/OrderPage.jsx
+++ src/pages/order/OrderPage.jsx
@@ -1,27 +1,14 @@
 import Main from '../../components/ui/Main/Main';
 import PageSection from '../../components/PageSection/index';
-import DeliveryForm from '../../components/DeliveryForm/DeliveryForm';
-import PurchaseCard from '../../components/PurchaseCard/PurchaseCard';
-import T from '../../assets/images/black_t.jpg';
 import styled from 'styled-components';
 import Wrap from '../../components/ui/Wrap/Wrap';
+import OrderPageContent from '../../components/OrderPageContent/OrderPageContent';
 const OrderPage = () => {
-  const userCartData = [
-    {
-      productId: 1,
-      product_name: '티셔츠',
-      product_img: T,
-      count: 1,
-      price: 10000,
-      option: 's',
-    },
-  ];
   return (
     <Main>
       <PageSection titleText="주문하기">
         <OrderContent>
-          <DeliveryForm />
-          <PurchaseCard userCartData={userCartData} />
+          <OrderPageContent />
         </OrderContent>
       </PageSection>
     </Main>
